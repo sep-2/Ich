5cfedeaaced9b7ed8c5188a8667cf413f14cb464
kurehajime <xiidec@gmail.com>
2025-10-19 18:36:34 +0900

    テストコード追加

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -3,14 +3,25 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+// Forward declaration for the function implemented in Sample.cpp.
+int Add(int left, int right);
+
 namespace UnitTest
 {
-	TEST_CLASS(UnitTest)
+	TEST_CLASS(SampleTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Add_ReturnsSumForPositiveValues)
 		{
+			Assert::AreEqual(5, Add(2, 3));
+			Assert::AreEqual(0, Add(0, 0));
+		}
+
+		TEST_METHOD(Add_HandlesNegativeValues)
+		{
+			Assert::AreEqual(-1, Add(2, -3));
+			Assert::AreEqual(-5, Add(-2, -3));
 		}
 	};
 }
--- UnitTest/pch.h
+++ UnitTest/pch.h
@@ -1,12 +1,10 @@
-﻿// pch.h: プリコンパイル済みヘッダー ファイルです。
-// 次のファイルは、その後のビルドのビルド パフォーマンスを向上させるため 1 回だけコンパイルされます。
-// コード補完や多くのコード参照機能などの IntelliSense パフォーマンスにも影響します。
-// ただし、ここに一覧表示されているファイルは、ビルド間でいずれかが更新されると、すべてが再コンパイルされます。
-// 頻繁に更新するファイルをここに追加しないでください。追加すると、パフォーマンス上の利点がなくなります。
+// pch.h: precompiled header for test project
+// Converted to UTF-8 to avoid encoding issues.
 
 #ifndef PCH_H
 #define PCH_H
 
-// プリコンパイルするヘッダーをここに追加します
+#include <ciso646>  // alternative operator keywords (not, and, or)
 
-#endif //PCH_H
+#endif // PCH_H
+
